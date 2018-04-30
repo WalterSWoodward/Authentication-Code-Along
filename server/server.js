7e3a42821757513720c85ab5cef222ce5d8b38c4
Walter Woodward <walter.sekope.woodward@gmail.com>
2018-04-30 14:57:00 -0400

    comment added on question about nodemon

--- server/server.js
+++ server/server.js
@@ -25,3 +25,9 @@ server.get('/', function(req, res) {
 });
 
 server.listen(5000, () => console.log('\n=== API on port 5k ===\n'));
+
+// Class Question: Why don't you just run `yarn nodemon server.js` in the command line?
+// Luis' Answer:  You could do this.  You would have to be sure that you installed nodemon globally though.
+// As it is, you just install nodemon as a dev dependency and then type in the script 'start' command,
+// and then you can just type `yarn start` to start your server.  Doing it this way means that you do
+// not need to have nodemon globally installed on your system.
