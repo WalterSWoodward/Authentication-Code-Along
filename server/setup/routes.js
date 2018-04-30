c32deb80a6ceb07ff09b81eccb8d2ef8b2dcf6d3
Walter Woodward <walter.sekope.woodward@gmail.com>
2018-04-30 19:01:23 -0400

    pseudo-code for makeToken
    currently breaking code cuz jsonwebtoken is not installed as a dependency

--- server/setup/routes.js
+++ server/setup/routes.js
@@ -1,5 +1,19 @@
 const User = require('../users/User');
 
+function makeToken(user) {
+    // return token
+    // sub: subject (id)
+    // Time stamp so we know when the document was created and when it will expire.
+    const timestamp = new Date().getTime();
+
+    // In here we are going to configure our Token, the way we want to sign it.
+    const jwt.sign({
+        sub: user._id,
+        username: user.username,
+        // we don't want to send the password back!
+    })
+}
+
 module.exports = function(server) {
   server.get('/', function(req, res) {
     res.send({ api: 'up and running' });
@@ -11,7 +25,8 @@ module.exports = function(server) {
     const credentials = req.body;
     // add user to database
     const user = new User(credentials);
-    user.save().then(inserted => { 
+    user.save().then(inserted => {
+      const token = makeToken(inserted);
       res.status(201).json(inserted);
     });
   });
