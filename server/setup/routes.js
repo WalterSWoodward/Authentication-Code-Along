e02f92c3f130dc5c6a5304fae8626bf93b584679
Walter Woodward <walter.sekope.woodward@gmail.com>
2018-04-30 15:26:33 -0400

    began to write server.post
    we need to import our model, but we have not written it yet, so we'll do that next

--- server/setup/routes.js
+++ server/setup/routes.js
@@ -2,4 +2,12 @@ module.exports = function(server) {
   server.get('/', function(req, res) {
     res.send({ api: 'up and running' });
   });
+
+  //
+  server.post('/api/register', function(req, res) {
+    // grabs username and password from Body
+    const credentials = req.body;
+    // add user to database
+    res.send({ api: 'up and running' });
+  });
 };
