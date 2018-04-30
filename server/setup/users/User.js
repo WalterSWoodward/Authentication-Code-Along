e40f612eafd457f191ba9c6fe78196878ddbe6e6
Walter Woodward <walter.sekope.woodward@gmail.com>
2018-04-30 15:33:09 -0400

    User.js created and completed
    basic userSchema written and ready to be deployed

---
+++ server/setup/users/User.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true
+    },
+    password: {
+        type: String,
+        required: true,
+        minLength: 4,
+    },
+});
+
+// The third parameter here is not required as 'User' will be lowercased and pluralized
+// automatically as the title of the collection.  It is added here just to be more explicit.
+module.exports = mongoose.model('User', userSchema, 'users')
