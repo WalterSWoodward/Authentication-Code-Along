56e62415bb315721a44362624f21deb9e6d17eb6
Walter Woodward <walter.sekope.woodward@gmail.com>
2018-04-30 20:01:38 -0400

    `register` complete - token up and running
    post now returns { token } successfully

--- server/setup/routes.js
+++ server/setup/routes.js
@@ -2,19 +2,25 @@ const User = require('../users/User');
 const jwt = require('jsonwebtoken');
 
 function makeToken(user) {
-    // return token
-    // sub: subject (id)
-    // Time stamp so we know when the document was created and when it will expire.
-    const timestamp = new Date().getTime();
-
-    // In here we are going to configure our Token, the way we want to sign it.
-    // https://jwt.io/introduction/
-   return jwt.sign({
-        sub: user._id, 
-        username: user.username,
-        // we don't want to send the password back!
-        race: user.race
-    })
+  // return token
+  // sub: subject (id)
+  // Time stamp so we know when the document was created and when it will expire.
+  const timestamp = new Date().getTime();
+  const payload = {
+    // sub: subject (id) who the token is about
+    sub: user._id,
+    username: user.username,
+    // we don't want to send the password back!
+    race: user.race,
+    // "issued at time"
+    iat: timestamp
+  };
+  const secret = 'no size limit on tokens';
+  const options = { expiresIn: '4h' };
+  // In here we are going to configure our Token, the way we want to sign it.
+  // https://jwt.io/introduction/
+  // https://www.npmjs.com/package/jsonwebtoken
+  return jwt.sign(payload, secret, options);
 }
 
 module.exports = function(server) {
@@ -30,7 +36,7 @@ module.exports = function(server) {
     const user = new User(credentials);
     user.save().then(inserted => {
       const token = makeToken(inserted);
-      res.status(201).json(inserted);
+      res.status(201).json({ token });
     });
   });
 };
