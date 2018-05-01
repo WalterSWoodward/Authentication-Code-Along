6cc3b8543011c9773026c0621537bd85ff10ad5c
Walter Woodward <walter.sekope.woodward@gmail.com>
2018-04-30 21:32:54 -0400

    localStrategy (passport-local) added to routes.js

--- server/setup/routes.js
+++ server/setup/routes.js
@@ -1,6 +1,14 @@
-const User = require('../users/User');
-const jwt = require('jsonwebtoken');
+// Libraries
+const jwt = require('jsonwebtoken'); // https://www.npmjs.com/package/jsonwebtoken
 
+const passport = require('passport'); // https://www.npmjs.com/package/passport
+// Will use this to generate middleware
+const LocalStrategy = require('passport-local'); // https://www.npmjs.com/package/passport-local
+
+// User.js with userSchema, bcrypt hashing, and bycrypt password compare
+const User = require('../users/User');
+
+// token
 function makeToken(user) {
   // return token
   // sub: subject (id)
@@ -23,6 +31,49 @@ function makeToken(user) {
   return jwt.sign(payload, secret, options);
 }
 
+// passport-local looks for username and password by default ^see docs
+// `done` here is kind of like `next`
+// this is all 'callback' syntax, versus `promise` syntax (then, catch) or `async`
+const localStrategy = new LocalStrategy(function(username, password, done) {
+  User.findOne({ username }, function(err, user) {
+    if (err) {
+      done(err);
+    }
+    if (!user) {
+      // If I don't find the user, I will assume that we were not able to validate
+      // their credentials.
+      // null-- as in 'no errors'
+      done(null, false);
+    }
+    // If you successfully get into here, then password IS valid, thus the user CAN be authenticated
+    user.verifyPassword(password, function(err, isValid) {
+      if (err) {
+        return done(err);
+      }
+      if (isValid) {
+        const { _id, username, race } = user;
+        return done(null, { _id, username, race }); // placed on req
+      }
+      return done(null, false);
+
+    // use strategies
+    passport.use(localStrategy);
+
+    // From passport-local docs -- notice .use is combined while we split it up above, then pass into as a variable
+    // passport.use(new LocalStrategy(
+    //     function(username, password, done) {
+    //       User.findOne({ username: username }, function (err, user) {
+    //         if (err) { return done(err); }
+    //         if (!user) { return done(null, false); }
+    //         if (!user.verifyPassword(password)) { return done(null, false); }
+    //         return done(null, user);
+    //       });
+    //     }
+    //   ));
+    });
+  });
+});
+
 module.exports = function(server) {
   server.get('/', function(req, res) {
     res.send({ api: 'up and running' });
@@ -40,8 +91,8 @@ module.exports = function(server) {
   });
 
   server.post('/api/login', (req, res) => {
-      // find user using the creds from the body
-      // check the verify password with what we have stored
-      // if condition passes, then issue token to user - which will enable them to access resources
-  })
+    // find user using the creds from the body
+    // check the verify password with what we have stored
+    // if condition passes, then issue token to user - which will enable them to access resources
+  });
 };
